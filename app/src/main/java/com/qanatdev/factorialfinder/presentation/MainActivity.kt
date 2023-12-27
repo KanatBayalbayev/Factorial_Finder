3ff90d4562161a12b0d8ff76bb0f5d0f460465e5
Kanat Bayalbayev <Kanatkz07@mail.ru>
2023-12-27 10:14:37 +0600

    created welcome screen and made some refactoring

--- app/src/main/java/com/qanatdev/factorialfinder/presentation/MainActivity.kt
+++ app/src/main/java/com/qanatdev/factorialfinder/presentation/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.qanatdev.factorialfinder.presentation
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -35,27 +38,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         findFactorial()
         observeMainViewModel()
+        launchWelcomeActivity()
 
 
     }
-    private fun findFactorial(){
-        with(binding){
+
+    private fun launchWelcomeActivity() {
+        val prefs: SharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val firstRun: Boolean = prefs.getBoolean(KEY_FIRST_RUN, true)
+
+        if (firstRun) {
+            startActivity(Intent(this, WelcomeActivity::class.java))
+        }
+    }
+
+
+    private fun findFactorial() {
+        with(binding) {
             buttonToFindFactorial.setOnClickListener {
                 val userInput = userInput.text.toString().trim()
                 mainViewModel.getFactorialNumber(userInput)
             }
         }
     }
-    private fun observeMainViewModel(){
-        mainViewModel.mainViewModelState.observe(this){
-            with(binding){
-                when(it){
+
+    private fun observeMainViewModel() {
+        mainViewModel.mainViewModelState.observe(this) {
+            with(binding) {
+                when (it) {
                     is ViewModelState.Loading -> {
                         Log.d("TestFor", "Loading...")
                     }
+
                     is ViewModelState.Error -> {
                         Log.d("TestFor", "Error...")
                     }
+
                     is ViewModelState.FactorialNumber -> {
                         Log.d("TestFor", "The result is ${it.factorialNumber}")
                         result.text = it.factorialNumber
@@ -66,7 +84,10 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
+    companion object {
+        private val PREFS_NAME = "MyPrefsFile"
+        private val KEY_FIRST_RUN = "firstRun"
+    }
 
 
 }
---
+++ app/src/main/java/com/qanatdev/factorialfinder/presentation/WelcomeActivity.kt
@@ -0,0 +1,31 @@
+package com.qanatdev.factorialfinder.presentation
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.qanatdev.factorialfinder.R
+import com.qanatdev.factorialfinder.databinding.ActivityWelcomeBinding
+
+class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityWelcomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val prefs: SharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        binding.buttonStart.setOnClickListener {
+            val editor: SharedPreferences.Editor = prefs.edit()
+            editor.putBoolean(KEY_FIRST_RUN, false)
+            editor.apply()
+            finish()
+        }
+    }
+
+    companion object{
+        private val PREFS_NAME = "MyPrefsFile"
+        private val KEY_FIRST_RUN = "firstRun"
+    }
+}
